f14affff45947d9dc9f97330f79d35375d45b260
Burak Yavas <burak.yavas@sanlab.net>
2022-02-01 16:44:42 +0300

    Edited program.cs.

--- DapperWithEF/Program.cs
+++ DapperWithEF/Program.cs
@@ -1,5 +1,21 @@
+using DataAccess.Abstract;
+using DataAccess.Dapper;
+using DataAccess.EntityFramework.Contexts;
+using DataAccess.Extensions;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+services.InitDatabaseConnection<ProjectDbContext>(db =>
+{
+    db.AddDataAccessor();
+});
+
 var app = builder.Build();
+
 
 app.MapGet("/", () => "Hello World!");
 
