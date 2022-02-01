9889712825bf477cd46bd95fb3f0c7e5f75b55ac
Burak Yavas <burak.yavas@sanlab.net>
2022-02-01 16:42:54 +0300

    Added DataController for testing.

---
+++ DapperWithEF/Controllers/DataController.cs
@@ -0,0 +1,14 @@
+﻿using Dapper;
+using DataAccess.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperWithEF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        
+    }
+}
