1a3b8b2afb7ac7e30aaf9d185c64f8616bd7952c
Burak Yavas <burak.yavas@sanlab.net>
2022-02-01 16:41:52 +0300

    Added SqlServerDbContext.

---
+++ DataAccess/EntityFramework/Contexts/SqlServerDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.EntityFramework.Contexts
+{
+    public class SqlServerDbContext : ProjectDbContext
+    {
+        public SqlServerDbContext(DbContextOptions<SqlServerDbContext> options, IConfiguration configuration) : base(options, configuration)
+        {
+            ConnectionString = Configuration.GetConnectionString("SqlServerConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                base.OnConfiguring(optionsBuilder.UseSqlServer(ConnectionString));
+            }
+        }
+    }
+}
