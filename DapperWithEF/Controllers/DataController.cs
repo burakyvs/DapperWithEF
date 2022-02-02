2fa9051cb8ac11f64493f74f4f6f49e3bb52a887
Burak Yavas <burak.yavas@sanlab.net>
2022-02-02 13:28:43 +0300

    Edited program.cs and datacontroller.cs to test.

--- DapperWithEF/Controllers/DataController.cs
+++ DapperWithEF/Controllers/DataController.cs
@@ -1,5 +1,6 @@
 ﻿using Dapper;
 using DataAccess.Abstract;
+using Entity;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,6 +10,32 @@ namespace DapperWithEF.Controllers
     [ApiController]
     public class DataController : ControllerBase
     {
-        
+        private readonly IDataAccessor<Product> _dataAccessor;
+        private readonly IDbConnector _dbConnector;
+        public DataController(IDataAccessor<Product> dataAccessor, IDbConnector dbConnector)
+        {
+            _dataAccessor = dataAccessor;
+            _dbConnector = dbConnector;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var ticks = DateTime.Now.Ticks;
+
+            using (var connection = await _dbConnector.OpenConnectionAsync())
+            {
+                using (var transaction = await _dbConnector.BeginTransaction(connection))
+                {
+                    await _dataAccessor.AddAsync(connection, new Product { Name = $"TestProduct-{ticks}", Price = 34.5M });
+
+                    await transaction.CommitAsync();
+
+                    var allProducts = await _dataAccessor.GetAllAsync(connection);
+
+                    return Ok(allProducts);
+                }
+            }
+        }
     }
 }
--- DapperWithEF/Program.cs
+++ DapperWithEF/Program.cs
@@ -1,16 +1,19 @@
-using DataAccess.Abstract;
-using DataAccess.Concrete.Dapper;
 using DataAccess.EntityFramework.Contexts;
 using DataAccess.Extensions;
-
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
 
+
+services.AddControllers();
+
+// Change the ProjectDbContext below, if you want to use another DbContext. 
+
 services.InitDatabaseConnection<ProjectDbContext>(db =>
 {
+    db.Configuration = builder.Configuration;
     db.AddDataAccessor();
 });
 
@@ -18,5 +21,12 @@ var app = builder.Build();
 
 
 app.MapGet("/", () => "Hello World!");
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
