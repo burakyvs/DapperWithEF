d44fa7e16dcba07af069bdebc0c2239ccba46baf
Burak Yavas <burak.yavas@sanlab.net>
2022-04-19 09:39:33 +0300

    Authentication & Authorization added.

--- DapperWithEF/Controllers/DataController.cs
+++ DapperWithEF/Controllers/DataController.cs
@@ -1,11 +1,13 @@
 ﻿using DataAccess.Abstract;
 using Entity;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DapperWithEF.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class DataController : ControllerBase
     {
         private readonly IDataAccessor<Product> _dataAccessor;
@@ -17,7 +19,24 @@ namespace DapperWithEF.Controllers
         }
 
         [HttpGet]
+        [Authorize("read")]
         public async Task<IActionResult> Get()
+        {
+            var ticks = DateTime.Now.Ticks;
+
+            using (var connection = await _dbConnector.OpenConnectionAsync())
+            {
+                var allProducts = await _dataAccessor.GetAllAsync(connection);
+
+                await _dbConnector.CloseConnectionAsync(connection);
+
+                return Ok(allProducts);
+            }
+        }
+
+        [Authorize("write")]
+        [HttpPost("add")]
+        public async Task<IActionResult> Add([FromBody]Product product)
         {
             var ticks = DateTime.Now.Ticks;
 
@@ -25,13 +44,34 @@ namespace DapperWithEF.Controllers
             {
                 using (var transaction = await _dbConnector.BeginTransaction(connection))
                 {
-                    await _dataAccessor.AddAsync(transaction, new Product { Name = $"TestProduct-{ticks}", Price = 34.5M });
+                    await _dataAccessor.AddAsync(transaction, product);
 
                     await transaction.CommitAsync();
 
-                    var allProducts = await _dataAccessor.GetAllAsync(connection);
+                    await _dbConnector.CloseConnectionAsync(connection);
 
-                    return Ok(allProducts);
+                    return Ok(product);
+                }
+            }
+        }
+
+        [Authorize("write")]
+        [HttpPost("addrange")]
+        public async Task<IActionResult> Add([FromBody] List<Product> products)
+        {
+            var ticks = DateTime.Now.Ticks;
+
+            using (var connection = await _dbConnector.OpenConnectionAsync())
+            {
+                using (var transaction = await _dbConnector.BeginTransaction(connection))
+                {
+                    await _dataAccessor.AddRangeAsync(transaction, products);
+
+                    await transaction.CommitAsync();
+
+                    await _dbConnector.CloseConnectionAsync(connection);
+
+                    return Ok(products);
                 }
             }
         }
--- DapperWithEF/Program.cs
+++ DapperWithEF/Program.cs
@@ -1,20 +1,38 @@
 using DataAccess.EntityFramework.Contexts;
 using DataAccess.Extensions;
-
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
+var configuration = builder.Configuration;
 
+services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.Authority = configuration["IdentityServerURL"];
+                        options.RequireHttpsMetadata = false;
+                        options.Audience = "product_service_api";
+                    });
+
+services.AddAuthorization(options =>
+{
+    options.AddPolicy("read",
+        policy => policy.RequireClaim("scope", "product.api.read", "product.api.full"));
+    options.AddPolicy("write",
+        policy => policy.RequireClaim("scope", "product.api.write", "product.api.full"));
+    options.AddPolicy("full",
+        policy => policy.RequireClaim("scope", "product.api.full"));
+});
 
 services.AddControllers();
 
 // If you want to use another DbContext, change the ProjectDbContext below. 
 
-services.InitDatabaseConnection<MySqlDbContext>(db =>
+services.InitDatabaseConnection<SqlServerDbContext>(db =>
 {
     db.Configuration = builder.Configuration;
-    db.AddDataAccessor();
+    db.AddDataAccessor(); // Default is Dapper.
 });
 
 var app = builder.Build();
@@ -23,6 +41,9 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
